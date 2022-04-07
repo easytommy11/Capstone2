b57dc36f180d980095bec24d43065dc6732e8f8a
Tomiwa Babalola <tbabalola004@gmail.com>
2022-04-07 09:06:18 -0400

    fiolknv

---
+++ Capstone2/LogInViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LogInViewController.swift
+//  SeniorCapstone2
+//
+//  Created by Jeffrey Voltaire on 2/28/22.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpElements()
+        // Do any additional setup after loading the view.
+    }
+    func setUpElements() {
+            errorLabel.alpha = 0
+            Utilities.styleTextField(firstNameTextField)
+            Utilities.styleTextField(lastNameTextField)
+            Utilities.styleFilledButton(loginButton)
+        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+}
